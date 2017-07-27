b14fd7af280e3e86dba91d4a61a2a34d00651e06
dduvall <don@deduvall.com>
2017-07-26 17:07:09 -0700

    updates

--- nimph-compiler/NimphVM.cs
+++ nimph-compiler/NimphVM.cs
@@ -8,9 +8,17 @@ namespace nimph_compiler
     public class NimphVM
     {
 
+
         public class Scope
         {
+            public Dictionary<String, String> symbols;
+        }
 
+        public List<Scope> _Symbols;
+
+        public NimphVM()
+        {
+            _Symbols = new List<Scope>();
         }
 
     }
